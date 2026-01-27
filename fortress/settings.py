0c3437542d0de97a1ff1b2399c6759491abc9e43
python5vivek <vm627850@gmail.com>
2026-01-27 11:31:44 +0530

    Deploy Mode On

--- fortress/settings.py
+++ fortress/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--e%t(lb159w5czv1!o7&r3@l#^u+$o+fhjwk^cksu@nus8c-=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
